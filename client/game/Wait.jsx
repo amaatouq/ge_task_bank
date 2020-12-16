ff85189344cd34c550b4d1ce7278c651722550a4
Hardi Suputra <jiksdi12@gmail.com>
2020-12-16 20:59:30 +0800

    fix: add warning on click quit (#18)
    * fix: add cofirm box modal
    
    * fix: update player

--- client/game/Wait.jsx
+++ client/game/Wait.jsx
@@ -1,40 +1,73 @@
 import React from "react";
 import Button from "../components/Button";
+import ConfirmBox from "../components/CofirmBox";
 
 export default class Wait extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      confirmShow: false,
+    };
+
+    this.handleQuit = this._handleQuit.bind(this);
+    this.handleYes = this._handleYes.bind(this);
+    this.handleCancel = this._handleCancel.bind(this);
+  }
+
+  _handleQuit() {
+    this.setState({
+      confirmShow: true,
+    });
+  }
+  _handleYes() {
+    const { player } = this.props;
+    this.setState({
+      confirmShow: false,
+    });
+    player.exit("playerQuit");
+  }
+  _handleCancel() {
+    this.setState({
+      confirmShow: false,
+    });
+  }
   render() {
     const { player, game } = this.props;
     const {
       treatment: { quitEarly },
     } = game;
+    const { confirmShow } = this.state;
 
     return (
-      <div className="flex h-full justify-center items-center text-gray-500">
-        <div className="w-96">
-          <div>
-            Press CONTINUE when you are ready to go to the next round
-            {quitEarly &&
-              `, or press QUIT if you've had enough and want to exit the experiment`}
-            .
-          </div>
-          <div className="mt-8 flex">
-            <Button
-              tick
-              onClick={() => player.stage.submit()}
-              text="CONTINUE"
-            />
-            <div className="ml-4">
-              {quitEarly && (
-                <Button
-                  tick
-                  onClick={() => player.exit("playerQuit")}
-                  text="QUIT"
-                />
-              )}
+      <>
+        <div className="flex h-full justify-center items-center text-gray-500">
+          <div className="w-96">
+            <div>
+              Press CONTINUE when you are ready to go to the next round
+              {quitEarly &&
+                `, or press QUIT if you've had enough and want to exit the experiment`}
+              .
+            </div>
+            <div className="mt-8 flex">
+              <Button
+                tick
+                onClick={() => player.stage.submit()}
+                text="CONTINUE"
+              />
+              <div className="ml-4">
+                {quitEarly && (
+                  <Button tick onClick={this.handleQuit} text="QUIT" />
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        {confirmShow && (
+          <ConfirmBox onYes={this.handleYes} onCancel={this.handleCancel}>
+            Are you sure?
+          </ConfirmBox>
+        )}
+      </>
     );
   }
 }
