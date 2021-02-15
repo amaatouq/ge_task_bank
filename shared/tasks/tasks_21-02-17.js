e7cf119d10adde41afa24120e88e46384a781c3b
Samuel Dupret <samu.dupret@hotmail.com>
2021-02-15 15:53:14 +0000

    new version of the NBA questions

--- shared/tasks/tasks_21-02-17.js
+++ shared/tasks/tasks_21-02-17.js
@@ -13,7 +13,7 @@ export const taskData = [
             teamA: 'San Antonio Spurs',
             teamB: 'Cleveland Cavaliers',
             dateEvent: 'Wednesday, February 17th',
-            dateHints: 'Thursday, February 11th, 7:00 AM ET',
+            dateHints: 'Monday, February 15th, 11:00 AM ET',
             season: '2020-2022',
 
             min: '0',
@@ -24,15 +24,15 @@ export const taskData = [
 
 
             text: 'How many points will the San Antonio Spurs score against the Cleveland Cavaliers on Wednesday, February 17th?',
-            description: 'All the Additional Information provided was accurate as of Thursday, February 11th, 7:00 AM ET',
+            description: 'See the Additional Information below! All the Additional Information provided was accurate as of Monday, February 15th, 11:00 AM ET',
 
             hints: [
                 'Last time they played in the 2019-2020 NBA regular season - The San Antonio Spurs (129 points) lost against the Cleveland Cavaliers (132 points)',
-                'Average points per game (PPG) this regular season - On average, the San Antonio Spurs score 110.2 points per game and, on average, the Cleveland Cavaliers score 103.6 points per game',
-                'Average opponent points per game (OPP PPG) this regular season - On average, teams score 111.4 points per game against the San Antonio Spurs and, on average, teams score 111.2 points per game against the Cleveland Cavaliers',
-                'Highest scoring players average points per game - The San Antonio Spurs highest scorer, DeMar DeRozan, has an average of 20.2 points per game and the Cleveland Cavaliers highest scorer, Collin Sexton, has an average of 22.7 points per game',
-                'Average 3-point field goals - The San Antonio Spurs have an average of 10.8 3-point field goals a game (~32.4 points) and the Cleveland Cavaliers have an average of 9.2 3-point field goals a game (~27.6 points)',
-                'Average 2-point field goals - The San Antonio Spurs have an average of 30.5 2-point field goals a game (~61 points) and the Cleveland Cavaliers have an average of 30.3 2-point field goals a game (~60.6 points)',
+                'Average points per game (PPG) this regular season - On average, the San Antonio Spurs score 111.1 points per game and, on average, the Cleveland Cavaliers score 104.1 points per game',
+                'Average opponent points per game (OPP PPG) this regular season - On average, teams score 111.5 points per game against the San Antonio Spurs and, on average, teams score 112.4 points per game against the Cleveland Cavaliers',
+                'Highest scoring players average points per game - The San Antonio Spurs highest scorer, DeMar DeRozan, has an average of 19.8 points per game and the Cleveland Cavaliers highest scorer, Collin Sexton, has an average of 22.7 points per game',
+                'Average 3-point field goals - The San Antonio Spurs have an average of 10.8 3-point field goals a game (~32.4 points) and the Cleveland Cavaliers have an average of 9 3-point field goals a game (~27 points)',
+                'Average 2-point field goals - The San Antonio Spurs have an average of 30.9 2-point field goals a game (~61.8 points) and the Cleveland Cavaliers have an average of 30.5 2-point field goals a game (~61 points)',
             ],
         },
     },
@@ -46,7 +46,7 @@ export const taskData = [
             teamA: 'Denver Nuggets',
             teamB: 'Washington Wizards',
             dateEvent: 'Wednesday, February 17th',
-            dateHints: 'Thursday, February 11th, 7:00 AM ET',
+            dateHints: 'Monday, February 15th, 11:00 AM ET',
             season: '2020-2023',
 
             min: '0',
@@ -57,15 +57,15 @@ export const taskData = [
 
 
             text: 'How many points will the Denver Nuggets score against the Washington Wizards on Wednesday, February 17th?',
-            description: 'All the Additional Information provided was accurate as of Thursday, February 11th, 7:00 AM ET',
+            description: 'See the Additional Information below! All the Additional Information provided was accurate as of Monday, February 15th, 11:00 AM ET',
 
             hints: [
                 'Last time they played in the 2019-2020 NBA regular season - The Denver Nuggets (114 points) lost against the Washington Wizards (128 points)',
-                'Average points per game (PPG) this regular season - On average, the Denver Nuggets score 116 points per game and, on average, the Washington Wizards score 113.9 points per game',
-                'Average opponent points per game (OPP PPG) this regular season - On average, teams score 111.9 points per game against the Denver Nuggets and, on average, teams score 120.9 points per game against the Washington Wizards',
-                'Highest scoring players average points per game - The Denver Nuggets highest scorer, Nikola Jokić, has an average of 26.9 points per game and the Washington Wizards highest scorer, Bradley Beal, has an average of 32.8 points per game',
-                'Average 3-point field goals - The Denver Nuggets have an average of 13.2 3-point field goals a game (~39.6 points) and the Washington Wizards have an average of 11.8 3-point field goals a game (~35.4 points)',
-                'Average 2-point field goals - The Denver Nuggets have an average of 30.1 2-point field goals a game (~60.2 points) and the Washington Wizards have an average of 29.3 2-point field goals a game (~58.6 points)',
+                'Average points per game (PPG) this regular season - On average, the Denver Nuggets score 115.5 points per game and, on average, the Washington Wizards score 112.5 points per game',
+                'Average opponent points per game (OPP PPG) this regular season - On average, teams score 111 points per game against the Denver Nuggets and, on average, teams score 119.1 points per game against the Washington Wizards',
+                'Highest scoring players average points per game - The Denver Nuggets highest scorer, Nikola Jokić, has an average of 26.5 points per game and the Washington Wizards highest scorer, Bradley Beal, has an average of 32.9 points per game',
+                'Average 3-point field goals - The Denver Nuggets have an average of 13.3 3-point field goals a game (~39.9 points) and the Washington Wizards have an average of 11.5 3-point field goals a game (~34.5 points)',
+                'Average 2-point field goals - The Denver Nuggets have an average of 29.7 2-point field goals a game (~59.4 points) and the Washington Wizards have an average of 29 2-point field goals a game (~58 points)',
             ],
         },
     },
@@ -79,7 +79,7 @@ export const taskData = [
             teamA: 'Houston Rockets',
             teamB: 'Philadelphia 76ers',
             dateEvent: 'Wednesday, February 17th',
-            dateHints: 'Thursday, February 11th, 7:00 AM ET',
+            dateHints: 'Monday, February 15th, 11:00 AM ET',
             season: '2020-2024',
 
             min: '0',
@@ -90,14 +90,14 @@ export const taskData = [
 
 
             text: 'How many points will the Houston Rockets score against the Philadelphia 76ers on Wednesday, February 17th?',
-            description: 'All the Additional Information provided was accurate as of Thursday, February 11th, 7:00 AM ET',
+            description: 'See the Additional Information below! All the Additional Information provided was accurate as of Monday, February 15th, 11:00 AM ET',
 
             hints: [
                 'Last time they played in the 2019-2020 NBA regular season - The Houston Rockets (96 points) lost against the Philadelphia 76ers (134 points)',
-                'Average points per game (PPG) this regular season - On average, the Houston Rockets score 109.9 points per game and, on average, the Philadelphia 76ers score 114.3 points per game',
-                'Average opponent points per game (OPP PPG) this regular season - On average, teams score 110.2 points per game against the Houston Rockets and, on average, teams score 110.2 points per game against the Philadelphia 76ers',
-                'Highest scoring players average points per game - The Houston Rockets highest scorer, James Harden, has an average of 24.8 points per game and the Philadelphia 76ers highest scorer, Joel Embiid, has an average of 29.4 points per game',
-                'Average 3-point field goals - The Houston Rockets have an average of 14.3 3-point field goals a game (~42.9 points) and the Philadelphia 76ers have an average of 10.6 3-point field goals a game (~31.8 points)',
+                'Average points per game (PPG) this regular season - On average, the Houston Rockets score 108.8 points per game and, on average, the Philadelphia 76ers score 114.1 points per game',
+                'Average opponent points per game (OPP PPG) this regular season - On average, teams score 110.3 points per game against the Houston Rockets and, on average, teams score 110.8 points per game against the Philadelphia 76ers',
+                'Highest scoring players average points per game - The Houston Rockets highest scorer, James Harden, has an average of 24.8 points per game and the Philadelphia 76ers highest scorer, Joel Embiid, has an average of 29.6 points per game',
+                'Average 3-point field goals - The Houston Rockets have an average of 14 3-point field goals a game (~42 points) and the Philadelphia 76ers have an average of 10.4 3-point field goals a game (~31.2 points)',
                 'Average 2-point field goals - The Houston Rockets have an average of 25.2 2-point field goals a game (~50.4 points) and the Philadelphia 76ers have an average of 31 2-point field goals a game (~62 points)',
             ],
         },
@@ -112,7 +112,7 @@ export const taskData = [
             teamA: 'New Orleans Pelicans',
             teamB: 'Portland Trail Blazers',
             dateEvent: 'Wednesday, February 17th',
-            dateHints: 'Thursday, February 11th, 7:00 AM ET',
+            dateHints: 'Monday, February 15th, 11:00 AM ET',
             season: '2020-2025',
 
             min: '0',
@@ -123,15 +123,15 @@ export const taskData = [
 
 
             text: 'How many points will the New Orleans Pelicans score against the Portland Trail Blazers on Wednesday, February 17th?',
-            description: 'All the Additional Information provided was accurate as of Thursday, February 11th, 7:00 AM ET',
+            description: 'See the Additional Information below! All the Additional Information provided was accurate as of Monday, February 15th, 11:00 AM ET',
 
             hints: [
                 'Last time they played in the 2019-2020 NBA regular season - The New Orleans Pelicans (128 points) won against the Portland Trail Blazers (115 points)',
-                'Average points per game (PPG) this regular season - On average, the New Orleans Pelicans score 112.5 points per game and, on average, the Portland Trail Blazers score 114.5 points per game',
-                'Average opponent points per game (OPP PPG) this regular season - On average, teams score 112.5 points per game against the New Orleans Pelicans and, on average, teams score 115.3 points per game against the Portland Trail Blazers',
-                'Highest scoring players average points per game - The New Orleans Pelicans highest scorer, Zion Williamson, has an average of 23.8 points per game and the Portland Trail Blazers highest scorer, Damian Lillard, has an average of 29.4 points per game',
-                'Average 3-point field goals - The New Orleans Pelicans have an average of 11.5 3-point field goals a game (~34.5 points) and the Portland Trail Blazers have an average of 16.2 3-point field goals a game (~48.6 points)',
-                'Average 2-point field goals - The New Orleans Pelicans have an average of 30 2-point field goals a game (~60 points) and the Portland Trail Blazers have an average of 24.4 2-point field goals a game (~48.8 points)',
+                'Average points per game (PPG) this regular season - On average, the New Orleans Pelicans score 113.2 points per game and, on average, the Portland Trail Blazers score 115.5 points per game',
+                'Average opponent points per game (OPP PPG) this regular season - On average, teams score 114.1 points per game against the New Orleans Pelicans and, on average, teams score 115.2 points per game against the Portland Trail Blazers',
+                'Highest scoring players average points per game - The New Orleans Pelicans highest scorer, Zion Williamson, has an average of 24.4 points per game and the Portland Trail Blazers highest scorer, Damian Lillard, has an average of 29.2 points per game',
+                'Average 3-point field goals - The New Orleans Pelicans have an average of 11.6 3-point field goals a game (~34.8 points) and the Portland Trail Blazers have an average of 16.1 3-point field goals a game (~48.3 points)',
+                'Average 2-point field goals - The New Orleans Pelicans have an average of 30.1 2-point field goals a game (~60.2 points) and the Portland Trail Blazers have an average of 24.7 2-point field goals a game (~49.4 points)',
             ],
         },
     },
@@ -145,7 +145,7 @@ export const taskData = [
             teamA: 'LA Clippers',
             teamB: 'Utah Jazz',
             dateEvent: 'Wednesday, February 17th',
-            dateHints: 'Thursday, February 11th, 7:00 AM ET',
+            dateHints: 'Monday, February 15th, 11:00 AM ET',
             season: '2020-2026',
 
             min: '0',
@@ -156,15 +156,15 @@ export const taskData = [
 
 
             text: 'How many points will the LA Clippers score against the Utah Jazz on Wednesday, February 17th?',
-            description: 'All the Additional Information provided was accurate as of Thursday, February 11th, 7:00 AM ET',
+            description: 'See the Additional Information below! All the Additional Information provided was accurate as of Monday, February 15th, 11:00 AM ET',
 
             hints: [
                 'Last time they played in the 2019-2020 NBA regular season - The LA Clippers (100 points) lost against the Utah Jazz (106 points)',
-                'Average points per game (PPG) this regular season - On average, the LA Clippers score 115 points per game and, on average, the Utah Jazz score 114.6 points per game',
-                'Average opponent points per game (OPP PPG) this regular season - On average, teams score 108 points per game against the LA Clippers and, on average, teams score 105.8 points per game against the Utah Jazz',
-                'Highest scoring players average points per game - The LA Clippers highest scorer, Kawhi Leonard, has an average of 26.5 points per game and the Utah Jazz highest scorer, Donovan Mitchell, has an average of 24 points per game',
-                'Average 3-point field goals - The LA Clippers have an average of 14.6 3-point field goals a game (~43.8 points) and the Utah Jazz have an average of 17 3-point field goals a game (~51 points)',
-                'Average 2-point field goals - The LA Clippers have an average of 26.8 2-point field goals a game (~53.6 points) and the Utah Jazz have an average of 23.7 2-point field goals a game (~47.4 points)',
+                'Average points per game (PPG) this regular season - On average, the LA Clippers score 115.8 points per game and, on average, the Utah Jazz score 115 points per game',
+                'Average opponent points per game (OPP PPG) this regular season - On average, teams score 108 points per game against the LA Clippers and, on average, teams score 105.7 points per game against the Utah Jazz',
+                'Highest scoring players average points per game - The LA Clippers highest scorer, Kawhi Leonard, has an average of 26.7 points per game and the Utah Jazz highest scorer, Donovan Mitchell, has an average of 24.2 points per game',
+                'Average 3-point field goals - The LA Clippers have an average of 14.5 3-point field goals a game (~43.5 points) and the Utah Jazz have an average of 16.7 3-point field goals a game (~50.1 points)',
+                'Average 2-point field goals - The LA Clippers have an average of 27.3 2-point field goals a game (~54.6 points) and the Utah Jazz have an average of 24 2-point field goals a game (~48 points)',
             ],
         },
     },
