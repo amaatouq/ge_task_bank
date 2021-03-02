b2499c3f0d7505fbab44ba600e7e559a7e1ed5ab
Samuel Dupret <samu.dupret@hotmail.com>
2021-03-02 11:05:24 +0000

    add more styling to social instructions

--- client/components/round/SocialInstructions.jsx
+++ client/components/round/SocialInstructions.jsx
@@ -11,15 +11,17 @@ export default class SocialInstructions extends Component {
 
         return (
 
-            <div className="mb-20 pl-12 text-xl text-gray-600">
+            <div className="mb-20 p-12 text-xl text-gray-700 bg-gray-100 rounded-md">
                 <p>This is a social stage where you can:</p>
 
                 {interactionMode === "continuous" && <>
                     <ul className={"list-disc pl-6"}>
                         {!hideSocialNumeric && <li>See information about the other players' responses (even if they change them)</li>}
                         {chat && <li>Chat with other players about their response</li>}
-                        <li>Modify your response until you click 'submit' or the stage ends</li>
+                        <li>Modify your response until you click 'submit' (cannot modify it once you click submit!) or the stage ends</li>
                     </ul>
+                    <br />
+                    <p>If everyone clicks 'submit', the stage will end early and you will go on to the next stage.</p>
                 </>
                 }
 
@@ -28,6 +30,8 @@ export default class SocialInstructions extends Component {
                         {!hideSocialNumeric && <li>See information about the other players' responses</li>}
                         {chat && <li>Chat with other players about their response</li>}
                     </ul>
+                    <br />
+                    <p>If everyone clicks 'OK', the stage will end early and you will go on to the next stage.</p>
                 </>
                 }
             </div>
