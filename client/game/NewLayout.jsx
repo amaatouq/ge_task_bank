009990da9d0f1a460cb84a324a489a361857a127
Hardi SUputra <jiksdi12@gmail.com>
2021-03-30 11:33:48 +0800

    fix: update grid for main new layout

--- client/game/NewLayout.jsx
+++ client/game/NewLayout.jsx
@@ -26,9 +26,12 @@ export default class NewLayout extends React.Component {
 
     return (
       <div className="h-full text-base alt-main-container">
-        <div className="bg-red-500"></div>
-        <div className="bg-green-500"></div>
-        <div className="bg-blue-500"></div>
+        <div className=" question-container">
+          <div className="question-container-column bg-white w-full bg-green-500">
+            <div className="w-full bg-gray-500 h-full"></div>
+          </div>
+        </div>
+        <div className="bg-blue-500 meta-container"></div>
       </div>
     );
   }
