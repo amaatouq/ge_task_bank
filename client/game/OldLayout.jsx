1d9f46b3cd7fcea97232815599e687e46b6ff593
Hardi SUputra <jiksdi12@gmail.com>
2021-03-30 11:17:54 +0800

    fix: update main layout

--- client/game/OldLayout.jsx
+++ client/game/OldLayout.jsx
@@ -1,26 +1,14 @@
-import React, { Fragment } from "react";
-import DebugButtons from "../components/DebugButtons";
+import React from "react";
 import Wait from "../components/Wait";
-import { Avatar } from "./Avatar";
 import Response from "./Response";
-import Score from "./Score";
-import Timer from "./Timer";
 
 export default class OldLayout extends React.Component {
   render() {
     const {
       round,
       stage,
-      player,
       game: {
-        treatment: {
-          feedback,
-          playerCount,
-          hideAvatar,
-          hideTimer,
-          chat,
-          hideSocialNumeric = false,
-        },
+        treatment: { chat, hideSocialNumeric = false },
       },
     } = this.props;
 
@@ -30,31 +18,7 @@ export default class OldLayout extends React.Component {
     const cols = `${hasQImage ? "1fr" : ""} 1fr ${has3rdcol ? "320px" : ""}`;
 
     return (
-      <div className="h-full text-base main-container">
-        <header className="h-16	bg-gray-200 grid grid-cols-3 items-center px-6">
-          <div>
-            {playerCount > 0 && !hideAvatar ? (
-              <Avatar bordered player={player} />
-            ) : (
-              <Fragment></Fragment>
-            )}
-          </div>
-          {(stage.name === "wait") | hideTimer ? (
-            <div></div>
-          ) : (
-            <Timer {...this.props} />
-          )}
-          <div className="flex justify-end items-center">
-            <DebugButtons {...this.props} />
-            {feedback && <Score player={player} />}
-          </div>
-        </header>
-
-        {/* Why did we show the avatar twice? */}
-        {/* <div className="w-full bg-gray-50 py-4 px-6">
-          <div>{playerCount > 0 && <Avatar bordered player={player} />}</div>
-        </div> */}
-
+      <>
         {stage.name === "wait" ? (
           <Wait {...this.props} />
         ) : (
@@ -65,7 +29,7 @@ export default class OldLayout extends React.Component {
             <Response {...this.props} />
           </section>
         )}
-      </div>
+      </>
     );
   }
 }
