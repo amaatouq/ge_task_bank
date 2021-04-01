e61c02ce8d9b0255c5e359823b1d231d54698191
Hardi SUputra <jiksdi12@gmail.com>
2021-04-01 14:55:52 +0800

    feat add question layout

---
+++ client/components/question/Questions.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+
+export class Question extends Component {
+  render() {
+    return (
+      <div className="alt-question-container h-full">
+        <div className="question-image-area">
+          <div className="img-area w-100 bg-gray-500 h-full"></div>
+        </div>
+        <div className="question-detail">
+          <div className="question-wrapper overflow-x-auto">
+            <div className="flex ln-27">
+              <div className="flex-initial mr-2">
+                <strong>Question: </strong>
+              </div>
+              <div className="flex-initial">
+                What is the correlation between the X and Y variable in this
+                scatter plot?
+              </div>
+            </div>
+            <div className="flex-1 w-full pl-2 text-gray-700 leading-5">
+              The Armed Conflict Location & Event Data Project (ACLED) is a
+              non-governmental organization that tracks violent conflict in
+              Asia, the Middle East, and Africa. One type of event they track is
+              those where civilians were intentionally targeted.
+              <div>
+                The Armed Conflict Location & Event Data Project (ACLED) is a
+                non-governmental organization that tracks violent conflict in
+                Asia, the Middle East, and Africa. One type of event they track
+                is those where civilians were intentionally targeted.
+              </div>
+              <div>
+                The Armed Conflict Location & Event Data Project (ACLED) is a
+                non-governmental organization that tracks violent conflict in
+                Asia, the Middle East, and Africa. One type of event they track
+                is those where civilians were intentionally targeted.
+              </div>
+              <div>
+                The Armed Conflict Location & Event Data Project (ACLED) is a
+                non-governmental organization that tracks violent conflict in
+                Asia, the Middle East, and Africa. One type of event they track
+                is those where civilians were intentionally targeted.
+              </div>
+              <div>
+                The Armed Conflict Location & Event Data Project (ACLED) is a
+                non-governmental organization that tracks violent conflict in
+                Asia, the Middle East, and Africa. One type of event they track
+                is those where civilians were intentionally targeted.
+              </div>
+              <div>
+                The Armed Conflict Location & Event Data Project (ACLED) is a
+                non-governmental organization that tracks violent conflict in
+                Asia, the Middle East, and Africa. One type of event they track
+                is those where civilians were intentionally targeted.
+              </div>
+              <div>
+                The Armed Conflict Location & Event Data Project (ACLED) is a
+                non-governmental organization that tracks violent conflict in
+                Asia, the Middle East, and Africa. One type of event they track
+                is those where civilians were intentionally targeted.
+              </div>
+              <div>
+                The Armed Conflict Location & Event Data Project (ACLED) is a
+                non-governmental organization that tracks violent conflict in
+                Asia, the Middle East, and Africa. One type of event they track
+                is those where civilians were intentionally targeted.
+              </div>
+            </div>
+            <div className="w-full ln-27">
+              <strong>More info:</strong>
+            </div>
+            <div className="w-full pl-2 text-gray-700 leading-5">
+              In 2018, they recorded 841 such events in Somalia.
+            </div>
+            <div className="w-full ln-27">
+              <strong>Update your response below to the right.</strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- client/game/NewLayout.jsx
+++ client/game/NewLayout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Question } from "../components/question/Questions";
 
 export default class NewLayout extends React.Component {
   render() {
     return (
       <div className="h-full text-base alt-main-container">
-        <div className=" question-container">
-          <div className="question-container-column bg-white w-full bg-green-500">
-            <div className="w-full bg-gray-500 h-full"></div>
+        <div className="question-container">
+          <div className="question-container-column bg-white w-full">
+            <div className="w-full h-full">
+              <Question />
+            </div>
           </div>
         </div>
         <div className="bg-blue-500 meta-container"></div>
